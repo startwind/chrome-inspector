d45a8191697709a55e8fd4ebcacf0061642eabea
phmLabs <nils.langner@startwind.io>
2025-09-19 06:23:27 +0200

    update readme

--- checks/index.js
+++ checks/index.js
@@ -1,6 +1,6 @@
 import {StatusCodeCheck} from './general/StatusCodeCheck.js';
 import {SlowRequestCheck} from './general/SlowRequestCheck.js';
-import {LargeResponseCheck} from './general/LargeRequestCheck.js';
+import {LargeResponseCheck} from './general/LargeResponseCheck.js';
 import {InsecureRequestCheck} from "./http/InsecureRequestCheck.js";
 import {MissingCacheHeaderCheck} from "./http/MissingCacheHeaderCheck.js";
 import {Http11Check} from "./http/Http11Check.js";
--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 # Inspect - Chrome Extension
 
 ![Dev Tools Panel](docs/dev-tools.png)
+
+
+## Rules
+
+### General
+
+- Large Response (Content-Length > 1 MB)
+- Slow Request (Duration > 800 ms)
+- Status Code (>= 400)
+
+### HTTP
+
+- Insecure Request (HTTP)
+- Missing Cache Header (static resources)
+- HTTP/1.1 (instead of HTTP/2)
