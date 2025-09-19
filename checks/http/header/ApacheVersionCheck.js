ad4b00f996e622ff636dc5527616e292582cfb69
phmLabs <nils.langner@startwind.io>
2025-09-19 12:56:14 +0200

    ApacheVersionCheck

---
+++ checks/http/header/ApacheVersionCheck.js
@@ -0,0 +1,20 @@
+export class ApacheVersionCheck {
+    name = "ApacheVersionCheck";
+    severity = "medium";
+
+    async check(req) {
+        try {
+            const headers = req.responseHeaders || {};
+            const serverHeader = headers["server"] || headers["Server"];
+            if (serverHeader && /apache\/\d+/i.test(serverHeader)) {
+                return {
+                    message: `server header exposes Apache version: "${serverHeader}"`,
+                    severity: this.severity
+                };
+            }
+        } catch (e) {
+            // Fehler beim Header-Parsing ignorieren
+        }
+        return false;
+    }
+}
--- checks/index.js
+++ checks/index.js
@@ -4,6 +4,7 @@ import {LargeResponseCheck} from './general/LargeResponseCheck.js';
 import {InsecureRequestCheck} from "./http/InsecureRequestCheck.js";
 import {MissingCacheHeaderCheck} from "./http/MissingCacheHeaderCheck.js";
 import {Http11Check} from "./http/Http11Check.js";
+import {ApacheVersionCheck} from "./http/header/ApacheVersionCheck.js";
 
 export const checks = [
     new StatusCodeCheck(),
@@ -11,5 +12,8 @@ export const checks = [
     new LargeResponseCheck(),
     new InsecureRequestCheck(),
     new MissingCacheHeaderCheck(),
-    new Http11Check()
+    new Http11Check(),
+
+    // http -> header
+    new ApacheVersionCheck()
 ];
